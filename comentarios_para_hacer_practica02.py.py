11de930cf9a92b2b985d7c037a497a832cc343bc
Adri-Z <54725497+Adri-Z@users.noreply.github.com>
2019-09-19 20:46:05 -0500

    Create ej_manda_error.py
    Se vio qué es una estructura de try-except.
    Pero llegué trarde , así que al rato pongo bien el apunte.

---
+++ comentarios_para_hacer_practica02.py.py
@@ -0,0 +1,33 @@
+#CreadoPor:AIZCH
+#fECHA: 19/09/19
+
+Ayudantía.
+Notas sobre la tarea:
+
+1. Identar y corregir en el código que se da.
+2. error numerico
+3. 
+
+a=1
+b=2
+c=3
+
+(a,b)=(1,2)
+
+si a<b
+   (a,b)
+sino
+    (b,a)
+    
+   # Hay que hacer una comparación entre los números. 
+   #Para ir haciendo las comparaciones.
+   
+     a < b < c
+  o  b < c < a  
+  
+  # Luego si no sale...
+  Hay que pedirle al usuario otro valor de c, hasta que el número caiga dentro del interrrvalo.
+  
+  #Luego hay que hacer manejo de errores.
+  # Cn estructuras anidadas
+  # Por si el usuario ingresa otro tipo de dato.
